4a6901db4065bf07d4886dd61ec8a6497cf61219
aidenkhoury <aidenkhoury04@gmail.com>
2025-11-06 15:17:58 +1100

    feat express

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const { PORT } = require('./config');
+
+async function startServer() {
+    try {
+
+        // Start server
+        app.listen(PORT, () => {
+            console.log(`Server listening on PORT ${PORT}`);
+        })
+
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+startServer();
