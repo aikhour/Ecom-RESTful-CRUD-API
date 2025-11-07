0c5f0ae8e104e3f75320ada3d06adb682f650c90
aidenkhoury <aidenkhoury04@gmail.com>
2025-11-07 15:42:59 +1100

    feat create product

--- services/productService.js
+++ services/productService.js
@@ -5,5 +5,22 @@ const ProductModelInstance = new ProductModel();
 module.exports = class ProductService {
     // service to make use of the product functions
 
-    
+    /**
+     * POST create product
+     * @params {Object} data [New Product Data]
+     * @returns {Object|null} [New Product Record]
+     */
+    async createProduct(data) {
+
+        try {
+            // Check for Product record, otherwise creates new one
+            const product = await ProductModelInstance.createProduct(data);
+            
+            // if success
+            return product;
+            
+        } catch(error) {
+            throw error;
+        }
+    }    
 }
