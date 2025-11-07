e01009006f384537b58d61ada6b1f1a16d6aa156
aidenkhoury <aidenkhoury04@gmail.com>
2025-11-07 15:27:22 +1100

    feat product POST + PUT

---
+++ models/products.js
@@ -0,0 +1,74 @@
+const db = require('../db');
+const pgp = require('pg-promise')({ capSQL: true });
+
+module.exports = class ProductModel {
+
+    /**
+     * POST Create new product record
+     * @params {Object} data [Product record]
+     * @return {Object|null} [Created product]
+     */
+    async createProduct(data) {
+        try {
+            const { id } = data;
+
+
+            // generate create record statement
+            const statement = pgp.helpers.insert(data, null, 'product_table') + `RETURNING *`;
+            // execute statement
+            const result = await db.query(statement);
+
+            // return result if successful
+            if(result.rows?.length) {
+                return {
+                    message: `Product record created with id: ${id}`,
+                    record: result.rows[0]
+                }
+            }
+            
+            // returns null if unsuccessful
+            return null
+
+        } catch(error) {
+            throw new Error(error);
+        }
+
+
+    }
+
+    /**
+    * PUT Updates a product record
+     * @param {Object} data [product data]
+     * @returns {Object|null} [product record updated]
+     */
+    async updateProduct(data) {
+    try {  
+    // extract id and params from data
+    const { id, ...params } = data;
+    
+    // Generate SQL statements - pgp helper funcs
+    // condition for change
+    const condition = pgp.as.format('WHERE id = ${id} RETURNING *', {id});
+    // change all but user id
+    const statement = pgp.helpers.update(params, null, 'product_table') + condition;
+            
+    // execute statement
+    const result = await db.query(statement);
+    
+    // if successful
+    if(result.rows?.length) {
+        return {
+            message: `Product record with id: ${id} updated.`,
+            record: result.rows[0]
+        }
+    }
+    
+    // if not succcessful
+    return null;        
+    
+    } catch(error) {
+        throw new Error(error);
+    }
+    }
+
+}
