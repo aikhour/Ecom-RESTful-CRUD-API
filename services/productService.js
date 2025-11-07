9bc33cff36c3468ea608855d22ec8b99391a8db2
aidenkhoury <aidenkhoury04@gmail.com>
2025-11-07 15:40:41 +1100

    update product service

---
+++ services/productService.js
@@ -0,0 +1,9 @@
+const createError = require('http-errors');
+const ProductModel = require('../models/products');
+const ProductModelInstance = new ProductModel();
+
+module.exports = class ProductService {
+    // service to make use of the product functions
+
+    
+}
